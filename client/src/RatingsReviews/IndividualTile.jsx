1d586e01ab8671fc1f5c5aafd40d16ed2bd8795b
Charles Ewing <charlesewing3@gmail.com>
2021-09-13 16:19:25 -0400

    make date render in "Month DD, YYYY" format

--- client/src/RatingsReviews/IndividualTile.jsx
+++ client/src/RatingsReviews/IndividualTile.jsx
@@ -19,6 +19,15 @@ class IndividualTile extends React.Component {
     // call API to see if this user actually made a purchase, then pass this info to MyUsername
   }
 
+  reformatDate(date) {
+    var split = date.split('-');
+    var months = ["January","February","March","April","May","June","July","August","September","October","November","December"];
+    var day = split[2].slice(0, 2);
+    var month = months[split[1] - 1];
+    var year = split[0];
+    return `${month} ${day}, ${year}`;
+  }
+
   handleHelpfulClick() {
     if (!this.state.helpful) {
       this.state.helpful = true;
@@ -40,9 +49,11 @@ class IndividualTile extends React.Component {
     return (
       <div>
         {console.log(this.props.review)}
+        <br></br>
+        <br></br>
         <div>==> INDIVIDUAL TILE COMPONENT</div>
         <div>Star Rating Container. Rating: {this.props.review.rating}</div>
-        <div>Date: {this.props.review.date}</div>
+        <div>Date: {this.reformatDate(this.props.review.date)}</div>
         <b>Review Summary: {this.props.review.summary}</b>
         <p>Review Body(HANDLE PHOTOS): {this.props.review.body}</p>
         <IRecommend recommend={this.props.review.recommend}/>
