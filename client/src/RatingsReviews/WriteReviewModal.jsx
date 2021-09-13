46d31e2a5ad34b0627acccfe2aec05acc4ac2cd0
Charles Ewing <charlesewing3@gmail.com>
2021-09-13 12:57:47 -0400

    set up and hard-code WriteReviewModal form component

--- client/src/RatingsReviews/WriteReviewModal.jsx
+++ client/src/RatingsReviews/WriteReviewModal.jsx
@@ -10,6 +10,43 @@ class WriteReviewModal extends React.Component {
     return (
       <div>
         <div>WRITE REVIEW MODAL COMPONENT</div>
+        <h4>Write Your Review</h4>
+        <h5>About the [Product Name Here]</h5>
+        <form>
+          <input
+            name="Overall Rating"
+            type="text"
+            value="1, 2, 3, 4, 5 (star icons)"
+          />
+          <input
+            name="Do you recommend this product?"
+            type="text"
+            value="yes, no"
+          />
+          <input
+            name="Characteristics - 1"
+            type="radio"
+            value="1, 2, 3, 4, 5"
+          />
+          <input
+            name="Review Summary (optional)"
+            type="text"
+            value="Placeholder = 'Example: Best purchase ever!'"
+          />
+          <input
+            name="Review Body"
+            type="text"
+            value="Placeholder = 'Why did you like the product or not?' --> include character counter below"
+          />
+          <button>Upload your photos</button>
+          <input
+            name="what is your nickname?"
+          />
+          <input
+            name="Your email"
+          />
+          <button>Submit Review</button>
+        </form>
       </div>
     );
   }
