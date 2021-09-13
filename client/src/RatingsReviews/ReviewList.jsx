ac3cbc7c0100659defb5db063bfefe59d03e6080
Charles Ewing <charlesewing3@gmail.com>
2021-09-13 14:31:34 -0400

    create getAllReviews fn, call it inside componentDidMount

--- client/src/RatingsReviews/ReviewList.jsx
+++ client/src/RatingsReviews/ReviewList.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import IndividualTile from './IndividualTile.jsx';
 import axios from 'axios';
+import urlFragment from './urlFragment.jsx';
 
 
 class ReviewList extends React.Component {
@@ -16,12 +17,17 @@ class ReviewList extends React.Component {
 
   // GET all reviews, based on sortOption
   getAllReviews(product_id) {
-    axios.get();
+    axios.get(`${urlFragment}?product_id=${product_id}`)
+      .then(allReviews => {
+        this.setState({
+          allReviews: allReviews
+        });
+      })
+      .catch(err => console.error(err))
   }
 
   componentDidMount() {
-    console.log(this.props.product_id)
-    getAllReviews(this.props.product_id);
+    this.getAllReviews(this.props.product_id);
   }
   // when dynamically generating IndividualTiles, only include reviews that match the filterOptions prop
   render() {
