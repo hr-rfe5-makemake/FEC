ef78b21b0abb48aa35ddb6d3ca23fd8e94a10250
Charles Ewing <charlesewing3@gmail.com>
2021-09-15 12:59:19 -0400

    Recommendations 1.2.4.3 complete

--- client/src/RatingsReviews/BreakdownFilter.jsx
+++ client/src/RatingsReviews/BreakdownFilter.jsx
@@ -8,18 +8,31 @@ class BreakdownFilter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      metaData: []
+      metaData: [],
+      percentRecommend: null
     };
+    this.calcPercentRecommend = this.calcPercentRecommend.bind(this);
   }
 
   getMetaData(product_id) {
     axios.get(`${urlFragment}meta?product_id=${product_id}`)
     .then(metaData => {
+      console.log(metaData)
       this.setState({
         metaData: metaData
       });
+      this.calcPercentRecommend(metaData.data.recommended);
     })
     .catch(err => console.error(err))
+  }
+
+  calcPercentRecommend(recommended) {
+    var trueCount = parseInt(recommended[true]) || 0;
+    var falseCount = parseInt(recommended[false]) || 0;
+    var percentRecommend = Math.round(100 * trueCount / (falseCount + trueCount));
+    this.setState({
+      percentRecommend: percentRecommend
+    });
   }
 
   componentDidMount() {
@@ -32,6 +45,12 @@ class BreakdownFilter extends React.Component {
         <AverageRating
           metaData={this.state.metaData}
         />
+
+        {(!Array.isArray(this.state.metaData) && (this.state.metaData.data.recommended.false || this.state.metaData.data.recommended.true)) ?
+          <div id="percent-recommend">{this.state.percentRecommend}% of reviewers recommend this product</div>
+          : null
+        }
+
         <Breakdown
           filterOptions={this.props.filterOptions}
           metaData={this.state.metaData}
@@ -40,7 +59,6 @@ class BreakdownFilter extends React.Component {
         />
 
         <br></br>
-        <div>50% of reviewers recommend this product</div>
 
         <div>-->Product Breakdown (Factors)</div>
         <div>Size [SCALE]</div>
