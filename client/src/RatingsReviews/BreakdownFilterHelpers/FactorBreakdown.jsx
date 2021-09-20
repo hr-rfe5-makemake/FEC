f3f2aec9c61402df7841f7d9ce63e6a49656157f
Charles Ewing <charlesewing3@gmail.com>
2021-09-20 15:07:38 -0400

    make characteristic bars disappear if rating is NaN

--- client/src/RatingsReviews/BreakdownFilterHelpers/FactorBreakdown.jsx
+++ client/src/RatingsReviews/BreakdownFilterHelpers/FactorBreakdown.jsx
@@ -19,23 +19,25 @@ function FactorBreakdown({metaData}) {
       if (rating === 5) {
         rating -= 0.1;
       }
-      bars.push(
-        <div className="char-bar" key={characteristic}>
-          <div><b>{characteristic}</b></div>
-          <div className="bar-grey">
-            <div className="bar-mid" style={{height: "10px", width: '32%'}}></div>
-            <div className="bar-white" style={{height: "10px", width: '2%'}}></div>
-            <div className="bar-mid" style={{height: "10px", width: '32%'}}></div>
-            <div className="bar-white" style={{height: "10px", width: '2%'}}></div>
-            <div className="bar-mid" style={{height: "10px", width: '32%'}}></div>
-            <div className="bar-green-char" style={{height: "10px", width: `2%`, position: 'absolute', left:`${100 * (rating / 5)}%`}}></div>
+      if (!isNaN(rating)) {
+        bars.push(
+          <div className="char-bar" key={characteristic}>
+            <div><b>{characteristic}</b></div>
+            <div className="bar-grey">
+              <div className="bar-mid" style={{height: "10px", width: '32%'}}></div>
+              <div className="bar-white" style={{height: "10px", width: '2%'}}></div>
+              <div className="bar-mid" style={{height: "10px", width: '32%'}}></div>
+              <div className="bar-white" style={{height: "10px", width: '2%'}}></div>
+              <div className="bar-mid" style={{height: "10px", width: '32%'}}></div>
+              <div className="bar-green-char" style={{height: "10px", width: `2%`, position: 'absolute', left:`${100 * (rating / 5)}%`}}></div>
+            </div>
+            <div className="range">
+              <span>{words[characteristic][0]}</span>
+              <span>{words[characteristic][1]}</span>
+            </div>
           </div>
-          <div className="range">
-            <span>{words[characteristic][0]}</span>
-            <span>{words[characteristic][1]}</span>
-          </div>
-        </div>
-      );
+        );
+      }
     }
 
     return <div id="factor-bars">{bars}</div>;
