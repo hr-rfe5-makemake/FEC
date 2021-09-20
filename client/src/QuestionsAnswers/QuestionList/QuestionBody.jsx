70ae59507b95c7fa2cf2419c942913cc4390063a
Shavkat Shavkiev <shavkatshavkiev2002@gmail.com>
2021-09-20 19:31:53 -0400

    Fixed Search bug and Added a collapse questions button

--- client/src/QuestionsAnswers/QuestionList/QuestionBody.jsx
+++ client/src/QuestionsAnswers/QuestionList/QuestionBody.jsx
@@ -4,7 +4,7 @@ const QuestionBody = (props) => {
   if(!props.question.startOfWord && !props.question.searchTerm){
     return <span>{props.question.question_body}</span>
   } else {
-    return <span><span>{props.question.question_body.slice(0, props.question.startOfWord)}</span><span style={{backgroundColor: 'yellow'}}>{props.question.question_body.slice(props.question.startOfWord, props.question.searchTerm-1 + props.question.startOfWord)}</span><span>{props.question.question_body.slice(props.question.startOfWord + props.question.searchTerm-1)}</span></span>
+    return <span><span>{props.question.question_body.slice(0, props.question.startOfWord)}</span><span style={{backgroundColor: 'yellow'}}>{props.question.question_body.slice(props.question.startOfWord, (props.question.searchTerm + props.question.startOfWord))}</span><span>{props.question.question_body.slice(props.question.startOfWord + props.question.searchTerm)}</span></span>
   }
 }
 
