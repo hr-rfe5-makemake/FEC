f6dafae1c4943d2539477d1df9a4c399c0104867
Charles Ewing <charlesewing3@gmail.com>
2021-09-23 12:05:28 -0400

    deleted console.log; changed add review modal "add" button css

--- client/src/RatingsReviews/BreakdownFilter.jsx
+++ client/src/RatingsReviews/BreakdownFilter.jsx
@@ -18,7 +18,6 @@ class BreakdownFilter extends React.Component {
   getMetaData(product_id) {
     axios.get(`${urlFragment}reviews/meta?product_id=${product_id}`)
     .then(metaData => {
-      console.log(metaData)
       this.setState({
         metaData: metaData
       });
