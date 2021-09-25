0396b77768ccf036c18ba9a98e6c280d2eae1155
Ye Feng Chen <yefengchen96@gmail.com>
2021-09-25 11:35:12 -0400

    Added my linkedin

--- client/src/Footer.jsx
+++ client/src/Footer.jsx
@@ -4,12 +4,11 @@ import Search from './Search.jsx'
 
 const Footer = (props) => (
   <div id="page-footer">
-    {/* <div id="logo">MakeMake</div> */}
     <span id='footer-items'>
       <div>Charles Ewing<a href='https://github.com/charlesewing3' target='_blank'><i className="fab fa-github"></i></a><a href='https://www.linkedin.com/in/shavkat-shavkiev-0b8172201/' target='_blank'><i className="fab fa-linkedin"></i></a></div>
       <div>Christopher Hsu <a href='https://github.com/christopherchsu' target='_blank'><i className="fab fa-github"></i></a><a href='https://www.linkedin.com/in/shavkat-shavkiev-0b8172201/' target='_blank'><i className="fab fa-linkedin"></i></a></div>
       <div>Shavkat Shavkiev<a href='https://github.com/ShavkatShavkiev' target='_blank'><i className="fab fa-github"></i></a><a href='https://www.linkedin.com/in/shavkat-shavkiev-0b8172201/' target='_blank'><i className="fab fa-linkedin"></i></a></div>
-      <div>Ye Feng Chen<a href='https://github.com/yespacefeng' target='_blank'><i className="fab fa-github"></i></a><a href='https://www.linkedin.com/in/shavkat-shavkiev-0b8172201/' target='_blank'><i className="fab fa-linkedin"></i></a></div>
+      <div>Ye Feng Chen<a href='https://github.com/yespacefeng' target='_blank'><i className="fab fa-github"></i></a><a href='https://www.linkedin.com/in/ye-feng-chen/' target='_blank'><i className="fab fa-linkedin"></i></a></div>
     </span>
   </div>
 )
